b7fe1462ea89679968abadc91d774156729ee121
genuismasterr <ashishchugh2@hotmail.com>
2023-10-24 19:25:48 +0800

    did getCarparksByLocation on searchManager and SearchInterface, haven't test yet
    did getCarparksByLocation on searchManager and SearchInterface, haven't test yet

--- SearchInterface/searchInterface.js
+++ SearchInterface/searchInterface.js
@@ -10,6 +10,60 @@ router.get('/all', async(req, res) => {
     res.send(await searchManager.getAllInfo());
 })
 
+/*
+router.get('/nearby', async (req, res) => {
+    // Get the coordinates and radius from the request query parameters
+    const { longitude, latitude, radius } = req.query;
+
+    // Convert the coordinates to numbers
+    const coordinates = {
+        Long: parseFloat(longitude),
+        Lat: parseFloat(latitude)
+    };
+
+    // Call the getCarparksByLocation function to retrieve nearby car parks
+    const nearbyCarparks = await searchManager.getCarparksByLocation(coordinates, parseFloat(radius));
+
+    // Send the nearby car parks as the response
+    res.json(nearbyCarparks);
+});
+*/
+
+// Define a new route for retrieving nearby car parks
+router.get('/nearby', async (req, res) => {
+    try {
+        // Get the coordinates and radius from the request query parameters
+        const { longitude, latitude, radius } = req.query;
+
+        // Validate the parameters
+        if (!longitude || !latitude || !radius) {
+            return res.status(400).json({ error: "Longitude, latitude, and radius are required." });
+        }
+
+        const long = parseFloat(longitude);
+        const lat = parseFloat(latitude);
+        const rad = parseFloat(radius);
+
+        // Validate the parsed values
+        if (isNaN(long) || isNaN(lat) || isNaN(rad) || lat < -90 || lat > 90 || long < -180 || long > 180) {
+            return res.status(400).json({ error: "Invalid longitude, latitude, or radius values." });
+        }
+
+        const coordinates = {
+            Long: long,
+            Lat: lat
+        };
+
+        // Call the getCarparksByLocation function to retrieve nearby car parks
+        const nearbyCarparks = await searchManager.getCarparksByLocation(coordinates, rad);
+
+        // Send the nearby car parks as the response
+        res.json(nearbyCarparks);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 //create endpoint for fn
 //carparkManager.getAllnearbycarpacrks... and same for gettrendbycarparkid
 
--- SearchManager/searchManager.js
+++ SearchManager/searchManager.js
@@ -21,3 +21,43 @@ module.exports.initialiseTrends = async() => {
         })
     }
 }
+
+// Define a function to retrieve nearby car parks
+module.exports.getCarparksByLocation = async (coordinates, radius) => {
+    // Define a query to find car parks within the specified radius
+    const query = {
+        Coordinates: {
+            $nearSphere: {
+                $geometry: {
+                    type: "Point",
+                    coordinates: [coordinates.Long, coordinates.Lat]
+                },
+                $maxDistance: radius // Specify the maximum distance in meters
+            }
+        }
+    };
+
+    // Use MongoDB's find() method with the query to retrieve nearby car parks
+    const nearbyCarparks = await carparkInfoCollection.find(query).toArray();
+
+    return nearbyCarparks;
+};
+
+// DID all this, unsure if we want to keep this though:
+// 1- need to handle error for invalid query parameters
+// 2- Validate if the parsed longitude, latitude, and radius values are within a reasonable or expected range?
+// For instance, latitude should be between -90 and 90, and longitude should be between -180 and 180.
+// 3- If the radius is not provided, have a default value? Return Error?
+// 4- make geospatial query efficient by ensuring that there is an index on the Coordinates field in the MongoDB collection
+// 4- carparkInfoCollection.createIndex({ "Coordinates": "2dsphere" });
+// IMPT: will do 4 if it takes too long when testing
+
+// need to add:
+
+/*
+// This is usually placed where you are setting up your database connection or collection
+const setupDatabase = async () => {
+    // Assuming carparkInfoCollection is already connected/initialized here
+    await carparkInfoCollection.createIndex({ "Coordinates": "2dsphere" });
+};
+*/
